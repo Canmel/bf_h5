256d5135b92d2fc0b7524dbd41a2a3683f058f44
lidejian@skio.cn <lidejian@skio.cn>
2017-09-18 18:20:23 +0800

    ssss

--- js/main.js
+++ js/main.js
@@ -4,16 +4,6 @@ $(function(){
     $(".enter").click(function(){
         showPage("page02");
     });
-    $(".messageUl li").click(function(){
-        showPage("page03");
-        //滑动
-        var swiper = new Swiper('.swiper-container-h', {
-            paginationClickable: true,
-            nextButton: '.swiper-button-h',
-            prevButton: '.swiper-button-p',
-            loop: true,
-        });
-    });
     $(".speak").click(function(){
         showPage("page04");
     });
@@ -62,8 +52,33 @@ $(function(){
 
     $.get('http://chat.meedesidy.top/messages/show_all', {formats: 'json'}, function(data){
         $.each(data, function(index, item){
-            showMessaage(item.context, item.userid);
-        })
+            showMessaage(item.id, item.context, item.userid);
+        });
+        $(".messageUl li").click(function(){
+            showPage("page03");
+            //滑动
+            var swiper = new Swiper('.swiper-container-h', {
+                paginationClickable: true,
+                nextButton: '.swiper-button-h',
+                prevButton: '.swiper-button-p',
+                slideToClickedSlide:true,
+                loop: true,
+            });
+
+            var li = $(this);
+
+            $(".messageSlide").each(
+                function(index, item){
+                    if($(item).attr("message_id") == $(li).attr("message_id")){
+                       swiper.slideTo(index, 0)
+                    }
+                }
+            )
+        });
+
+
+
+        $('.swiper-pagination span').eq(2).trigger('click');
     });
 
     $("#bgmusic")[0].play();
@@ -75,21 +90,11 @@ function sendMessage() {
     });
 }
 
-function showMessaage(message, userid){
-    var messageSlideHtml = "<div class='swiper-slide messageSlide'><img src='img/tx0"+ Math.floor(Math.random()*6+1) +".png' class='txBig'><span class='nameBig'>" + userid + "</span><p class='messageBig'>" + message + "</p></div>";
-    var html = "<li><div class='tx'><img src='img/tx0"+ Math.floor(Math.random()*6+1) +".png'><span>" + userid + "</span></div><p class='message'>" + message + "</p><div class='clear'></div></li>"
+function showMessaage(id, message, userid){
+    var messageSlideHtml = "<div class='swiper-slide messageSlide' message_id="+ id +"><img src='img/tx0"+ Math.floor(Math.random()*6+1) +".png' class='txBig'><span class='nameBig'>" + userid + "</span><p class='messageBig'>" + message + "</p></div>";
+    var html = "<li message_id="+ id +"><div class='tx'><img src='img/tx0"+ Math.floor(Math.random()*6+1) +".png'><span>" + userid + "</span></div><p class='message'>" + message + "</p><div class='clear'></div></li>"
     $(".messageUl").append(html);
     $(".swiper-wrapper").append(messageSlideHtml);
-    $(".messageUl li").click(function(){
-        showPage("page03");
-        //滑动
-        var swiper = new Swiper('.swiper-container-h', {
-            paginationClickable: true,
-            nextButton: '.swiper-button-h',
-            prevButton: '.swiper-button-p',
-            loop: true,
-        });
-    });
 }
 
 function showPage(pageId){
